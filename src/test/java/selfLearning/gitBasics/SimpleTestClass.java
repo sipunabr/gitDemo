fb6bbb5202b9b15de247cfdeaa4583f33792e303
Abinash rath <sipun.abr@gmail.com>
2018-06-27 23:35:55 +0530

    Importing the git-demo project

---
+++ src/test/java/selfLearning/gitBasics/SimpleTestClass.java
@@ -0,0 +1,12 @@
+package selfLearning.gitBasics;
+
+import org.testng.annotations.Test;
+
+public class SimpleTestClass {
+	
+	@Test
+	public void printFunction() {
+		System.out.println("This is a simple test method");
+	}
+
+}
